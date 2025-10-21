af10033333887e02f9ee4adf7186116326477ed3
MauricioTdM <mauricio1tmelo@yahoo.com.br>
2025-10-21 00:20:13 -0300

    Fix

--- src/main/java/plataformaSaude/configuration/OAuth2Config.java
+++ src/main/java/plataformaSaude/configuration/OAuth2Config.java
@@ -7,6 +7,7 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -47,8 +48,10 @@ public class OAuth2Config {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .headers(headers -> headers.frameOptions().sameOrigin())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.sameOrigin())
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .oauth2Login(oauth2 -> oauth2
                         .defaultSuccessUrl("/home", true)
                         .failureUrl("/error")
